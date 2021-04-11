b60036ad39817e35d1199832999a47d317e56871
Aarón Parres López <aaronplmultimedia@gmail.com>
2021-04-11 23:14:43 +0200

    basic structure w/ buttons and texts

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Text('hello there')));
+    var questions = ['What\'s your favourite color?', 'Red', 'Green', 'Blue'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello there'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(questions[0]),
+              RaisedButton(
+                child: Text(questions[1]),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text(questions[2]),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text(questions[3]),
+                onPressed: null,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: ElevatedButton(
+          child: Text('hello'),
+          onPressed: test,
+        ),
+      ),
+    );
+  }
+
+  void test() {
+    print('test');
   }
 }
