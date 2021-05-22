05ab2720de199c76716ee919826672de9ce449bd
aparres <aparres@intercomempresas.com>
2021-05-23 01:29:02 +0200

    Splitting into smaller widgets Quizz + Results

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'question.dart';
-import 'answerButton.dart';
+import 'quizzModule.dart';
+import 'results.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,77 +12,64 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
 
   void _buttonPressedHandler() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+
     print(_questionIndex);
+    if (_questionIndex < _questions.length) print('We have more questions!!!');
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': [
-          'Blue',
-          'Red',
-          'Green',
-        ]
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': [
-          'Dog',
-          'Cat',
-          'Tortoise',
-        ]
-      },
-      {
-        'questionText': 'What\'s your favourite food?',
-        'answers': [
-          'Burger',
-          'Pizza',
-          'Tortilla de patatas',
-        ]
-      },
-      {
-        'questionText': 'What\'s your favourite tv show?',
-        'answers': [
-          'Jimmy Fallon\'s show',
-          'El Hormiguero',
-          'Jackass',
-        ]
-      },
-      {
-        'questionText': 'What\'s your favourite book?',
-        'answers': [
-          'Shadow and Bone',
-          'Hunger Games',
-          'Harry Potter',
-        ]
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('hello there'),
+          title: Text('QUIZZ APP'),
+          backgroundColor: Colors.teal.shade300,
         ),
         body: Center(
-          child: Column(
-            children: [
-              Question(questionText: questions[_questionIndex]['questionText']),
-              ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) => AnswerButton(
-                        answerCallbackFunc: _buttonPressedHandler,
-                        answerText: answer,
-                      ))
-                  .toList(),
-            ],
-          ),
+          child: _questionIndex < _questions.length
+              ? QuizzModule(
+                  questions: _questions,
+                  questionIndex: _questionIndex,
+                  buttonPressedHandler: _buttonPressedHandler)
+              : Results(),
         ),
         floatingActionButton: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.teal.shade300)),
           child: Text('hello there'),
           onPressed: () => print('Hello there'),
         ),
---
+++ lib/quizzModule.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'question.dart';
+import 'answerButton.dart';
+
+class QuizzModule extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function buttonPressedHandler;
+
+  QuizzModule(
+      {@required this.questions,
+      @required this.questionIndex,
+      @required this.buttonPressedHandler});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(questionText: questions[questionIndex]['questionText']),
+        ...(questions[questionIndex]['answers'] as List<String>)
+            .map((answer) => AnswerButton(
+                  answerCallbackFunc: buttonPressedHandler,
+                  answerText: answer,
+                ))
+            .toList(),
+      ],
+    );
+  }
+}
