e3522da38a2a15c5ca1c635bc04816873690b971
aparres <aparres@intercomempresas.com>
2021-05-23 16:41:20 +0200

    Two cards body structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('hello there'),
+          title: Text('PERSONAL EXPENSES'),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('Hello there'),
-            ],
-          ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              color: Colors.green,
+              child: Text('Transactions'),
+              elevation: 5,
+            ),
+          ],
         ),
         floatingActionButton: ElevatedButton(
           child: Text('hello there'),
