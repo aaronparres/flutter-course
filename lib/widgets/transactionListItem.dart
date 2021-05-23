6977e718bf120ac0ce3cb0c78293e1af021c4cea
aparres <aparres@intercomempresas.com>
2021-05-23 19:56:03 +0200

    Text styling inside the card

--- lib/widgets/transactionListItem.dart
+++ lib/widgets/transactionListItem.dart
@@ -10,6 +10,7 @@ class TransactionListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 3,
       child: Row(
         children: <Widget>[
           Container(
@@ -19,19 +20,37 @@ class TransactionListItem extends StatelessWidget {
             ),
             decoration: BoxDecoration(
               border: Border.all(
-                color: Colors.black,
+                color: Colors.blueGrey,
                 width: 2,
               ),
             ),
             padding: EdgeInsets.all(10),
             child: Text(
               '\$ ' + item.amount.toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.blueGrey,
+              ),
             ),
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(item.title),
-              Text(item.date.toString()),
+              Text(
+                item.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                ),
+              ),
+              Text(
+                item.date.toString(),
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey.shade700,
+                ),
+              ),
             ],
           ),
         ],
