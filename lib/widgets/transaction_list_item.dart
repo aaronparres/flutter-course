d1f2cf689fe97d750d1f8400469587685811a4b2
aparres <aparres@intercomempresas.com>
2021-05-24 16:30:04 +0200

    Round number to 2 decimals

--- lib/widgets/transaction_list_item.dart
+++ lib/widgets/transaction_list_item.dart
@@ -27,7 +27,7 @@ class TransactionListItem extends StatelessWidget {
             ),
             padding: EdgeInsets.all(10),
             child: Text(
-              '\$ ${item.amount.toString()}',
+              '\$ ${item.amount.toStringAsFixed(2)}',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
